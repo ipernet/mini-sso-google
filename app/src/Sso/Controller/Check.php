68b30bb79b95101ba639b5d4547e3acaeb2c4179
Guillaume Simon <ipernet@gmail.com>
2015-06-02 12:07:32 +0200

    JSONP added

--- app/src/Sso/Controller/Check.php
+++ app/src/Sso/Controller/Check.php
@@ -4,13 +4,26 @@ namespace Sso\Controller;
 
 use Silex\Application;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 
 class Check
 {
-	public function index(Application $app)
+	public function index(Request $request, Application $app)
 	{		
 		$user	=	$app['session']->get('user');
 	
-		return ! $user ? $app->abort(401) : (new JsonResponse())->setData($user);
+		if($user)
+		{
+			if($request->get('jsonp_callback') !== null)
+			{
+				$res	=	new JsonResponse($user);
+				
+				return $res->setCallback($request->get('jsonp_callback'));
+			
+			}
+			else
+				return new JsonResponse($user);
+		}
+		return $app->abort(401);
 	}
 }
